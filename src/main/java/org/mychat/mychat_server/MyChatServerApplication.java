2ec559d970a7548270c2eb5376c25fbd9e3992b6
CHANG LIU <120937649+changliu1122@users.noreply.github.com>
2023-05-08 17:36:10 +0200

    mychat server initialization

---
+++ src/main/java/org/mychat/mychat_server/MyChatServerApplication.java
@@ -0,0 +1,13 @@
+package org.mychat.mychat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyChatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mychat/mychat_server/MyChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mychat.mychat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
